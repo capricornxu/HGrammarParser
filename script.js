1fdcceb7ff0a34ea31af9e107300cdd3b41bb0fc
caprilancexu <caprilance.xu@foxmail.com>
2022-07-05 19:06:25 -0400

    allow empty space in quotes and allow no empty space arount brakets and parentheses

--- script.js
+++ script.js
@@ -35,49 +35,76 @@ $(document).ready(function(){
         return index;
     }
 
+    // replace element of specific index
     String.prototype.replaceAt = function(index, replacement) {
         return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+    }
+
+    function addESaround(string, symbol){
+        // console.log(symbol);
+        const index = allindexof(string, symbol);
+        console.log(index);
+        for(var i in index){
+            const index = allindexof(string, symbol);
+            if(index[i] >= 0){
+                if(string[index[i]-1] != ' '){
+                    string = string.slice(0, index[i]) + ' ' + string.slice(index[i]);
+                    index[i] = index[i] + 1;
+                }
+                // console.log(string[index[0]]);
+                if(string[index[i]+1] != ' '){
+                    string = string.slice(0, index[i]+1) + ' ' + string.slice(index[i]+1);
+                    index[i] = index[i] + 1;
+                }
+            }
+        }
+        return string;
     }
 
     $('#btn1').click(function(){
         var s = $('#txt').val();
 
         var tokenStream = s;
-        console.log(tokenStream);
-        var index = allindexof(tokenStream, "'")
-        console.log(index);
+
+        // replace the first element in quotes with #
+        var quote_index = allindexof(tokenStream, "'")
         for(var i in allindexof(tokenStream, "'")){
             if(i % 2 == 0){
-                tokenStream = tokenStream.replaceAt(index[i] + 1, "#");
+                tokenStream = tokenStream.replaceAt(quote_index[i] + 1, "#");
             }
         }
-        // if(tokenStream.indexOf("'") > 0){
-        //     tokenStream = tokenStream.replaceAt(tokenStream.indexOf("'") + 1, "#");
-        // }
-        console.log(tokenStream);
+
+        // tramsform quotes to 'string'
         tokenStream = tokenStream.split("'");
-        // console.log(tokenStream[1][0]);
         for(var i in tokenStream){
-            // tokenStream = tokenStream[i].trim();
             if(tokenStream[i][0] == '#'){
                 tokenStream[i] = 'string';
             }
         }
         tokenStream = tokenStream.join('');
+        // var test_token = tokenStream.slice();
+
+
+        // add empty space around brackets and parentheses
+        console.log(tokenStream);
+        tokenStream = addESaround(tokenStream, "[");
+        tokenStream = addESaround(tokenStream, "]");
+        tokenStream = addESaround(tokenStream, "(");
+        tokenStream = addESaround(tokenStream, ")");
+        console.log(tokenStream);
         tokenStream = tokenStream.trim().split(' ');
         console.log(tokenStream);
+        // console.log(tokenStream);
+        // tokenStream = tokenStream.trim().split(' ');
 
-        // detect number and string
+        // detect number
         for(var i in tokenStream){
             if(!isNaN(tokenStream[i])){
                 tokenStream[i] = 'number';
             }
         }
-        // console.log(tokenStream);
 
-        // var rules = $('#tgrm').val() + $('#ntgrm').val();
         const p = document.getElementById('test');
-        var test_rules = p.textContent;
         rules = p.textContent + $('#tgrm').val();
         // console.log(test_rules);
         rules = rules.trim().split('\n');
@@ -97,10 +124,6 @@ $(document).ready(function(){
             $('#dv').empty();
             // console.log(trees);
             $('#dv').append('<div class="tree" id="displayTree"><ul>' + displayTree(trees[0]) + '</ul></div></br>');
-            // for (var i in trees) {
-            //     // console.log(JSON.stringify(trees[i]))
-            //     $('#dv').append('<div class="tree" id="displayTree"><ul>' + displayTree(trees[i]) + '</ul></div></br>');
-            // }
         }
         else{
             $("#p1").html("<span style='color: red;'>Invalid</span>");
